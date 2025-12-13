2af4fab095f3d8bd064ee9ccea7897da30205610
prototype-xk <ebaloche@gaming.tech>
2025-12-13 21:14:01 +0100

    Finish Menu

--- Vrunneuse/Assets/scripts/Play.cs
+++ Vrunneuse/Assets/scripts/Play.cs
@@ -3,8 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class Play : MonoBehaviour
 {
+
+   public PlayerController playerController;
 public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
+public void BackScene(string BackSceneName)
+    {
+        playerController.IsPauseMenuOpen = false;
+        Time.timeScale = 1;
+        SceneManager.LoadScene(BackSceneName);
+    }
 }
+
--- Vrunneuse/Assets/scripts/PlayerController.cs
+++ Vrunneuse/Assets/scripts/PlayerController.cs
@@ -16,7 +16,7 @@ public class PlayerController : MonoBehaviour
     public float Gravity = 20f;
 
     public GameObject canvasPause;
-    private bool IsPauseMenuOpen;
+    public bool IsPauseMenuOpen=false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
