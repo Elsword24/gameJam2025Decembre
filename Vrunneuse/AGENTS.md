b20c7809507ec03264ad8e2390da1b09b7225574
prototype-xk <ebaloche@gaming.tech>
2025-12-13 15:38:13 +0100

    Menu Work

--- Vrunneuse/AGENTS.md
+++ Vrunneuse/AGENTS.md
@@ -1,4 +1,8 @@
 <!-- UNITY CODE ASSIST INSTRUCTIONS START -->
 - Project name: Vrunneuse
 - Unity version: Unity 6000.3.1f1
+- Active game object:
+  - Name: CreditManager
+  - Tag: Untagged
+  - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Vrunneuse/Assets/scripts/CanvaSwitcher.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvaSwitcher : MonoBehaviour
+{
+    public GameObject canvasMenuPrincipal;
+    public GameObject canvasCredit;
+    public Text CreditText;
+
+
+    void Start()
+    {
+        canvasMenuPrincipal.SetActive(true);
+        canvasCredit.SetActive(false);
+    }
+    public void SwitchToCredit()
+    {
+        Debug.Log("Go To Credit");
+        canvasMenuPrincipal.SetActive(false);
+        canvasCredit.SetActive(true);
+    }
+
+    public void SwitchToMenuPrincipale()
+    {
+        Debug.Log("Go To Menu");
+        canvasCredit.SetActive(false);
+        canvasMenuPrincipal.SetActive(true);
+    }
+}
