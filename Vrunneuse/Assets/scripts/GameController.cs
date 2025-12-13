c302d32dc087dc29f476bc739fce07c50b19514b
Quentin Launay <50755766+Elsword24@users.noreply.github.com>
2025-12-13 09:33:51 +0100

    add game controller

---
+++ Vrunneuse/Assets/scripts/GameController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    //Player start coordonnées
+    PlayerController playerController;
+    //Control de l'avatar
+    //Stockage des mouvements
+    //fin de tableau
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
