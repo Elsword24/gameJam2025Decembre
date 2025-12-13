94fdddad6d5d06983e8ec4052a2c28e08fa6fc1d
Elsword24 <quentinbaronlaunay@gmail.com>
2025-12-13 02:05:34 +0100

    add playerController and base gameplay

---
+++ Vrunneuse/Assets/scripts/PlayerController.cs
@@ -0,0 +1,111 @@
+using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    public float jumpForce = 1.5f;
+    Vector3 jump = new Vector3(0.0f, 2.0f, 0.0f);
+    public int jumpCharge = 1;
+    public float speed = 1.5f;
+    public float dashCharges = 1;
+    public float dashForce = 3;
+    private bool isDashing = false;
+    private bool isGrounded = false;
+    public float airControl = 0.2f;
+    public float Gravity = 20f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+
+    private float move;
+
+
+    void Update()
+    {
+        move = -Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Dash();
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.AddForce(Vector3.down * Gravity, ForceMode.Acceleration);
+
+
+        if (isDashing) return;
+
+        float control = isGrounded ? 1f : airControl;
+
+        Vector3 targetVelocity = new Vector3(
+            move * speed,
+            rigidbody.linearVelocity.y,
+            rigidbody.linearVelocity.z
+        );
+
+        rigidbody.linearVelocity = Vector3.Lerp(
+            rigidbody.linearVelocity,
+            targetVelocity,
+            control
+        );
+
+    }
+
+    void Jump()
+    {
+        if (jumpCharge != 0)
+        {
+            rigidbody.AddForce(jump * jumpForce, ForceMode.Impulse);
+            jumpCharge --;
+
+        }
+    }
+
+    void Dash()
+    {
+        if (dashCharges == 0 || isDashing) return;
+
+        isDashing = true;
+
+        Vector3 dashDirection = new Vector3(move, 0f, 0f);
+
+        if (dashDirection == Vector3.zero) dashDirection = transform.right;
+
+        rigidbody.AddForce(dashDirection.normalized * dashForce, ForceMode.Impulse);
+        dashCharges --;
+
+        Invoke(nameof(EndDash), 0.2f);
+    }
+
+    void EndDash()
+    {
+        isDashing = false;
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        isGrounded = true;
+        jumpCharge = 1;
+        dashCharges = 1;
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        isGrounded = false;
+    }
+}
+
