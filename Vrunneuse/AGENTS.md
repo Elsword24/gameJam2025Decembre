238c08b363a7e96099caa7590b626e8234181aed
Quentin Launay <50755766+Elsword24@users.noreply.github.com>
2025-12-14 15:57:49 +0100

    Finish build of the game

--- Vrunneuse/AGENTS.md
+++ Vrunneuse/AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: Vrunneuse
 - Unity version: Unity 6000.3.1f1
 - Active game object:
-  - Name: Cube
+  - Name: PauseMenu
   - Tag: Untagged
-  - Layer: Default
+  - Layer: UI
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Vrunneuse/Assets/scripts/UI/Resume.cs
+++ Vrunneuse/Assets/scripts/UI/Resume.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
 
-public class Resume : MonoBehaviour
+public class BackGame : MonoBehaviour
 {
     public GameObject canvasResume;
     public PlayerController playerController;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         canvasResume.SetActive(false);
     }
-
     public void ResumeGame()
     {
+        playerController.IsPauseMenuOpen = !playerController.IsPauseMenuOpen;
+        Time.timeScale = playerController.IsPauseMenuOpen ? 0f : 1f;  // Met en pause (0) ou reprend (1) le jeu
         canvasResume.SetActive(false);
     }
 }
